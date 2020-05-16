644672b1c17dbd17704f0c2b803382355273bcfe
hariswb <hariswb@gmail.com>
2020-05-17 04:01:59 +0700

    Updates

---
+++ precache-manifest.d4e6331a88842f2025dcc77777e72659.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03182a6b54338d0acc94e3f8f1ca861d",
+    "url": "/prosa/index.html"
+  },
+  {
+    "revision": "403cefb1328e328c7f25",
+    "url": "/prosa/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef59b42a48f8ae62b0f1",
+    "url": "/prosa/static/js/2.30955912.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prosa/static/js/2.30955912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403cefb1328e328c7f25",
+    "url": "/prosa/static/js/main.b7503f3a.chunk.js"
+  },
+  {
+    "revision": "e210c8eb792036f824da",
+    "url": "/prosa/static/js/runtime-main.d62a480d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prosa/static/media/logo.5d5d9eef.svg"
+  }
+]);
