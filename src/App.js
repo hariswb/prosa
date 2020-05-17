f629e539be8fc2c2b7858fe7c6a73df08ac0d9d2
hariswb <hariswb@gmail.com>
2020-05-17 20:50:32 +0700

    prosa init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as Prose from './prose.json'
+import * as About from './about.json'
+
+const about = About.description
+const proseList = Prose.list.map(x=><li key={x.id}><a href={x.url}>{x.title}</a><p>{x.date}</p></li>)
+console.log(proseList)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={"container"}>
+        <div className={"title"}>isinya cerpen</div>
+        <div className={"about"}><p>{About.description}</p></div>
+        <div className={"prose"}>
+          <ul>
+            {proseList}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
